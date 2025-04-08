42ed9f386fa67039b4d57f8f78252130319ae0f0
Masdakung <masdamankung@gmail.com>
2025-04-08 09:35:22 +0700

    Revert "remove-bootstrap-isRouteProvider"
    This reverts commit a8810b7bfb65e2588ec0e43619dd693ee5269b57.

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,4 +4,5 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\StyleProvider::class,
     App\Providers\VoltServiceProvider::class,
+    App\Providers\isRouteStyle::class,
 ];
